3f8239bb6aa26e64e8d4f1baa7e3b70cc06d33d5
Paul New <paul@freelyassociative.org>
2014-01-25 23:44:05 +0000

    * Added a new "switch platform" window as an entry point to the UniCache system. * Added some basic stuff to figure out which directories we should be working with while cacheing.

---
+++ UCPlatformSwitchWindow.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace UniCache
+{
+
+//-------------------------------------------------------------------------------------
+public class UCPlatformSwitchWindow : EditorWindow 
+{
+	static UCPlatformSwitchWindow Instance;
+	bool m_DoSwitch;
+	BuildTarget m_SelectedBuildTarget;
+
+	//*********************************************************************************
+	[MenuItem ("UniCache/Switch Platform")]
+	static void Init()
+	{
+		if(Instance == null)
+		{
+			Instance = (UCPlatformSwitchWindow)EditorWindow.GetWindow(typeof(UCPlatformSwitchWindow));
+		}
+	}
+
+	//*********************************************************************************
+	void OnGUI() 
+	{
+		Array targets = Enum.GetValues(typeof(BuildTarget));
+		foreach(BuildTarget build_target in targets)
+		{
+			if(GUILayout.Button(build_target.ToString()))
+			{
+				m_DoSwitch = true;
+				m_SelectedBuildTarget = build_target;
+				break;
+			}
+		}
+	}
+
+	//*********************************************************************************
+	void Update()
+	{
+		if(m_DoSwitch)
+		{
+			m_DoSwitch = false;
+			if(m_SelectedBuildTarget == EditorUserBuildSettings.activeBuildTarget)
+			{
+				Debug.Log("You're already using that target");
+			}
+			else
+			{
+				UCSwitcher.SwitchToBuildTarget(m_SelectedBuildTarget);
+			}
+
+		}
+	}
+}
+
+} // namespace UniCache
